9a9959b1b7a4214159ba4be3dba8c8485b77be53
wes.scoggin <wes.scoggin@wonderful.com>
2020-01-03 10:06:24 -0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ api = Api(app)
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'images'),'favicon.ico', mimetype='image/png')
+    return send_from_directory(os.path.join(app.root_path, 'Images'),'favicon.ico', mimetype='image/png')
 
 class getDisplay(Resource):
     # def get(self, settings):
