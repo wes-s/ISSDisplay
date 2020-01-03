9fd88b9b562eceb16e992e7a61f072cadc84a1b5
wes.scoggin <wes.scoggin@wonderful.com>
2020-01-03 10:02:03 -0800

    added favicon
    added route in app.py
    shortcut link in index.html
    favicon.ico in /images

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, request, render_template, make_response, send_file
 from flask_restful import Api, Resource
+from flask import send_from_directory
 from bokeh.embed import components
 from functions import *
+import os
 
 app = Flask(__name__)
 api = Api(app)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'images'),'favicon.ico', mimetype='image/png')
 
 class getDisplay(Resource):
     # def get(self, settings):
